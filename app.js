ffb0b7b2a6b829a3bc8baa4f10947e10db7338c7
ccastrom <claudiocastrom7@gmail.com>
2024-11-04 19:48:35 -0300

    changes: delete comments at 'app.js' add 'path' module into 'Server.js'

--- app.js
+++ app.js
@@ -3,34 +3,12 @@ const Server=require('./models/Server.js');
 
 
 require('dotenv').config();
-// var createError = require('http-errors');
-
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
 
 
 const server= new Server();
 
 server.startServer();
 
-// var connection= require('./database.js')
-
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
 
 
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
 
--- models/Server.js
+++ models/Server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('express');
+const path = require('path'); 
+
 
  
  class Server{
@@ -16,16 +18,20 @@ var cors = require('express');
 
     routes(){
         this.app.use( this.usersEndpoint,require('../routes/users.routes'))
+       
 
           
     
 
     }
     middlewares(){
+        this.app.set('views', path.join(__dirname, 'views'));
+        this.app.set('view engine', 'ejs');
         this.app.use(cors());
         this.app.use(express.json());
         this.app.use(express.urlencoded({ extended: true }))
         this.app.use(express.static ('public'));
+
        
     }
 
