601de218ed2b70ddcedec79be019db92be168476
ccastrom <claudiocastrom7@gmail.com>
2024-11-04 17:49:33 -0300

    change: deactivated database connection

--- database.js
+++ database.js
@@ -1,27 +1,27 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '123',
-  database : 'bautismo_iglesia'
-});
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+// var mysql      = require('mysql');
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : '123',
+//   database : 'bautismo_iglesia'
+// });
+// connection.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
 
 
-var SELECT_Query= ()=>{
-connection.query('SELECT * FROM PersonaBautismo ORDER BY id ASC LIMIT 5', function (error, results, fields) {
-  if (error) throw error;
-  results.forEach(element => {
-    console.log(element);
+// var SELECT_Query= ()=>{
+// connection.query('SELECT * FROM PersonaBautismo ORDER BY id ASC LIMIT 5', function (error, results, fields) {
+//   if (error) throw error;
+//   results.forEach(element => {
+//     console.log(element);
     
- });
-});
+//  });
+// });
  
-connection.end();
-}
+// connection.end();
+// }
 
 
-module.exports= {SELECT_Query}
+// module.exports= {SELECT_Query}
