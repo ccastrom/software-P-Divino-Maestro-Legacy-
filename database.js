87d4b5ca32b58991129fc9924c85462e04aa58b9
ccastrom <claudiocastrom7@gmail.com>
2024-11-04 23:35:59 -0300

    feat: add MySQL2 connection pool in 'database.js' for async/await, implement SELECT query in GET method for users endpoint

--- database.js
+++ database.js
@@ -1,23 +1,26 @@
 
 // Get the client
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 // Create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-   password : '123',
-  database: 'bautismo_iglesia',
-});
-
+   
+    const connection = mysql.createPool({
+      host: 'localhost',
+      user: 'root',
+      password : '123',
+      database : 'bautismo_iglesia'
+      // port: 3306,
+      // password: '',
+    });
+ 
 // A simple SELECT query
-connection.query(
-  'SELECT * FROM `Celebrante`',
-  function (err, results, fields) {
-    console.log(results); // results contains rows returned by server
-    console.log(fields); // fields contains extra meta data about results, if available
-  }
-);
+// connection.query(
+//   'SELECT * FROM `Celebrante`',
+//   function (err, results, fields) {
+//     console.log(results); // results contains rows returned by server
+//     console.log(fields); // fields contains extra meta data about results, if available
+//   }
+// );
 
 
 
